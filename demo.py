a2b5abd6190aeb70a251cd45b89edcd6eff17810
Tobias <tbp@kontrapunkt.com>
2019-10-30 11:34:34 +0100

    Added demo/example  files

---
+++ demo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8
+
+import configparser
+import datetime
+
+from kanboard import Kanboard
+
+import json2kanboard
+
+# Import configuration
+config = configparser.ConfigParser()
+config.read("demo.example.conf")
+
+# Create Kanboard API instance
+kanboard_instance = Kanboard(
+    config.get("kanboard","url"), 
+    config.get("kanboard","user"), 
+    config.get("kanboard","password") 
+)
+
+# Map roles in the JSON file to task owners
+roles = {
+  'ROLE_MANAGER': 'user_b'
+  }
+
+# Create Kanboard project
+json2kanboard.create_project(
+  "onboarding_project.demo.json",
+  kanboard_instance,
+  project_title = "TEST_PROJECT",
+  project_identifier = "TEST_PROJECT_ID",
+  due_date = datetime.date(2020, 1, 1),
+  roles = roles
+  )
