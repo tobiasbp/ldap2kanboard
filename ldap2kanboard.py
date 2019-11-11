c631b30a5d313390a04e388cbb8b122064798964
Tobias <tbp@kontrapunkt.com>
2019-11-11 15:38:05 +0100

    Added creation of offboarding projects

--- ldap2kanboard.py
+++ ldap2kanboard.py
@@ -55,11 +55,21 @@ con.bind()
 
 # FIXME: Move these to config file
 USER_START_DATE_FIELD = 'fdContractStartDate'
+USER_END_DATE_FIELD = 'fdContractEndDate'
 ONBOARDING_PROJECT_ID_PREFIX = 'ONBOARDING'
+OFFBOARDING_PROJECT_ID_PREFIX = 'OFFBOARDING'
 MY_TASKS_PROJECT_ID_PREFIX = 'MYTASKS'
+
+# Days to offboarding for types of users
+days_for_offboarding = {
+  "employee": 4*7,
+  "hours": 4*7,
+  "freelancer": 1*7
+}
 
 # The JSON file used for onboarding
 ONBOARDING_JSON = config.get("json", "onboarding")
+OFFBOARDING_JSON = config.get("json", "offboarding")
 MY_TASK_JSON = config.get("json", "my_tasks")
 
 # Search for users
@@ -72,6 +82,7 @@ con.search(
     'userPassword',
     'uidNumber',
     USER_START_DATE_FIELD,
+    USER_END_DATE_FIELD,
     'o',
     'title',
     'mail',
@@ -89,7 +100,7 @@ ldap_users_by_uid = { str(u.uid):u for u in con.entries }
 kb_users_by_username = { u['username']: u for u in kb.get_all_users() }
 
 # The time is now
-now = datetime.now(timezone.utc)
+now = datetime.date(datetime.now(timezone.utc))
 
 #################################
 # Sync LDAP users with Kanboard #
@@ -162,9 +173,9 @@ kb_users_by_username = { u['username']: u for u in kb.get_all_users() }
 # FIXME: Update groups in Kanboard based on data in LDAP
 
 
-############################
-# Kanboard users with LDAP #
-############################
+################################
+# Check Kanboard users in LDAP #
+################################
 '''
 # Should we delete users not in LDAP? Not if we have more than one sync like now!
 for u_uid, u_data in kb_users_by_username.items():
@@ -185,8 +196,7 @@ for u in con.entries:
     continue
 
   # User's start date
-  # FIXME: Non-pretty way og extracting value. Is there a better way?
-  u_start_date = u[USER_START_DATE_FIELD].values[0]
+  u_start_date = datetime.date(u[USER_START_DATE_FIELD].value)
 
   # If the user's start date is in the future
   if u_start_date > now:
@@ -243,7 +253,7 @@ for u in con.entries:
 
     else:
       # Warn if no manager
-      print("No manager found for user ''".format(u.cn))
+      logging.warning("No manager found for user ''".format(u.cn))
 
 
     # Project description with placeholders
@@ -271,6 +281,120 @@ for u in con.entries:
 
     # Log the completion of the project
     logging.info("Created onboarding project for '{}'"
+      .format(u.cn))
+
+###############################
+# Create offboarding projects #
+###############################
+
+for u in con.entries:
+
+  # Ignore user if no end date
+  if not u[USER_END_DATE_FIELD]:
+    continue
+
+  u_days_left = \
+    (u[USER_END_DATE_FIELD].value - datetime.now(timezone.utc)).days
+    #datetime.now(timezone.utc) - u[USER_END_DATE_FIELD].value
+
+
+  # Abort if we don't know the offboarding time for the user time 
+  if u.employeeType.value not in days_for_offboarding.keys():
+    logging.error("User type '{}' for user '{}' is undefined"
+      .format(u.employeeType, u.cn))
+    continue
+
+
+  # Is it time to offboard for this type of user?
+  if u_days_left <= days_for_offboarding[u.employeeType.value]:
+
+    # Create onboarding project identifier
+    project_identifier = OFFBOARDING_PROJECT_ID_PREFIX + str(u.uidNumber)
+
+    # Look for existing onboarding project
+    r = kb.get_project_by_identifier(
+      identifier = project_identifier
+      )
+
+    # Abort current iteration if project exists
+    if r:
+      logging.debug("Offboarding project exists for '{}'. Don't create."
+        .format(u.cn))
+      continue
+
+    print("User '{}({})' of type '{}' has end date in {} days."
+      .format(u.cn, u.uid, u.employeeType, u_days_left))
+
+    # The user's end date
+    u_end_date = datetime.date(u[USER_END_DATE_FIELD].value)
+
+    # Assume no roles
+    roles = {}
+
+    # Define placeholders
+    placeholders = {
+      'USER_NAME': u.cn.value,
+      'USER_UID': u.uid.value,
+      'USER_TITLE': u.title.value,
+      'USER_TYPE': u.employeeType.value,
+      'USER_COMPANY': u.o.value,
+      'USER_END_DATE': u_end_date.strftime('%d-%m-%Y'),
+      'USER_PRIVATE_MAIL': u.fdPrivateMail.value,
+      'USER_WORK_MAIL': u.mail.value
+      }
+
+    # Pattern for extracting uid from a DN
+    p = re.compile('uid=([a-z]+)')
+
+    # Match object. None if no match
+    m = p.match(u['manager'].value)
+
+    # If we have a match
+    if m:
+      # Extract the uid from the managers DN
+      u_manager_uid = m.group(1)
+
+      # Add manager role
+      roles['ROLE_MANAGER'] = u_manager_uid
+
+      # Add manager name to placeholders
+      placeholders['USER_MANAGER_NAME'] = \
+        ldap_users_by_uid[u_manager_uid]['cn'].value
+
+    else:
+      # Warn if no manager
+      logging.warning("No manager found for user ''".format(u.cn))
+
+
+    # Project description with placeholders
+    description = (
+      "* Name: USER_NAME (USER_UID)\n" +
+      "* Private email: USER_PRIVATE_MAIL\n" +
+      "* Work email: USER_WORK_MAIL\n" +
+      "* Company: USER_COMPANY\n" +
+      "* Title: USER_TITLE\n" +
+      "* End date: USER_END_DATE\n" +
+      "* Type: USER_TYPE\n" +
+      "* People manager: USER_MANAGER_NAME"
+      )
+
+    #print(roles)
+    #print(placeholders)
+    #print(project_identifier)
+
+    # Create the kanboard project
+    json2kanboard.create_project(
+      OFFBOARDING_JSON,
+      kb,
+      project_description = description,
+      project_identifier = project_identifier,
+      due_date = u_end_date,
+      roles = roles,
+      placeholders = placeholders
+      )
+
+    # Log the completion of the project
+    logging.info("Created offboarding project for '{}'"
       .format(u.cn))
 
 
@@ -321,7 +445,7 @@ for u in con.entries:
     }
 
   # Start date is now if start date is in the past
-  u_start_date = max(now, u[USER_START_DATE_FIELD].values[0])
+  u_start_date = max(now, u[USER_START_DATE_FIELD].value)
   
 
   # Create the kanboard project
